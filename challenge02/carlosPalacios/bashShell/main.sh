fe870c7e23d9163097ab68bee69a5473e750e799
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-11-12 16:49:16 -0400

    feat: add chicle while

--- challenge02/carlosPalacios/bashShell/main.sh
+++ challenge02/carlosPalacios/bashShell/main.sh
@@ -27,3 +27,21 @@ for (( i=0; i<$limit; i++ )); do
     randomNumber=$(shuf -i 1-100 -n 1) # Generate one random number
     numbers+=($randomNumber) # Add the number to the list
 done
+
+echo ${numbers[@]}
+
+amount=0
+
+while [ $amount -lt $limit ]; do
+    number=${numbers[$amount]}
+
+    # verify is the number is even
+    if [ $((number % 2)) -eq 0 ]; then
+        echo $number
+        break
+    else
+        echo "This number is odd"
+    fi
+
+    amount=$((amount + 1))
+done 
